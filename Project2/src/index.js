ca751e53b9ff9303e5f6026b2aeb1af7bb5b1e47
hastiafra <77177165+hastiafra@users.noreply.github.com>
2023-11-03 16:09:54 -0400

    set up

--- Project2/src/index.js
+++ Project2/src/index.js
@@ -1,50 +1,65 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as THREE from "three";
+import "./style/main.css";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-camera.position.z = 2
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 20;
+scene.add(camera);
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const canvas = document.querySelector(".webgl");
 
-new OrbitControls(camera, renderer.domElement)
+const renderer = new THREE.WebGLRenderer({ canvas }); //default renderer
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: 0x00ff00,
+});
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-window.addEventListener('resize', onWindowResize, false)
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const orbit = new OrbitControls(camera, canvas);
+
+renderer.render(scene, camera);
+
+// scene.background = new THREE.Color(0xff8800) //background color
+
+// const camera = new THREE.PerspectiveCamera(
+//     75,
+//     window.innerWidth / window.innerHeight,
+//     0.1,
+//     1000
+// )
+// camera.position.z = 2
+
+// renderer.setSize(window.innerWidth, window.innerHeight)
+// document.body.appendChild(renderer.domElement)
+
+window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function animate() {
-    requestAnimationFrame(animate)
+const loop = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+loop();
 
-    render()
-}
 
-function render() {
-    renderer.render(scene, camera)
-}
-
-animate()
